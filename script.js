39a565b849f63136c5ef1edf41af2a1c65967492
charlygus <64556151+charlygus@users.noreply.github.com>
2026-01-02 18:32:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 👇👇👇 AQUÍ ES DONDE TIENES QUE PEGAR TU CÓDIGO 👇👇👇
-const SHEET_ID = '1xY_89Gtg...q7L'; 
+const SHEET_ID = '1jMrd9A3Pvs-r606i8H6NYp6RAw-46rE5tlGfXUL0QK4';
 // 👆👆👆 Borra lo de dentro de las comillas y pon tu ID (ej: 1BxiMM...)
 
 let menuData = [];
